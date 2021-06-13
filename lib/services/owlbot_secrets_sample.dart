15ea5c0359f94c037042ff8dc5ae8e806fd18fb5
Korppi <3602291+Korppi@users.noreply.github.com>
2021-06-14 00:55:38 +0300

    better comments

--- lib/services/owlbot_secrets_sample.dart
+++ lib/services/owlbot_secrets_sample.dart
@@ -1,8 +1,8 @@
 /// Holds all secrets for OwlBot (only token)
 ///
 /// Only this sample file is commited to git without real token.
-/// After pull/fork you should enter your own token in this file and
-/// rename this to 'owlbot_secrets.dart'
+/// After pull/fork you should copy this file with new name "owlbot_secrets.dart"
+/// and add your token in there. That file is already gitignored.
 class OwlBotSecrets {
   static final String token = '';
 }
