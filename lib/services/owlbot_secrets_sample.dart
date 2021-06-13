dc84aebbf2d13e33a5a7cfaa264c93d796448d95
Korppi <3602291+Korppi@users.noreply.github.com>
2021-06-14 00:53:16 +0300

    Create owlbot_secrets_sample.dart

---
+++ lib/services/owlbot_secrets_sample.dart
@@ -0,0 +1,8 @@
+/// Holds all secrets for OwlBot (only token)
+///
+/// Only this sample file is commited to git without real token.
+/// After pull/fork you should enter your own token in this file and
+/// rename this to 'owlbot_secrets.dart'
+class OwlBotSecrets {
+  static final String token = '';
+}
