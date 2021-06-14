b58dadd28008f46038685cdaf5583e70b91c711e
Korppi <3602291+Korppi@users.noreply.github.com>
2021-06-14 11:17:45 +0300

    Owlbot can now call API and return Word object from successfull response

--- lib/services/owlbot.dart
+++ lib/services/owlbot.dart
@@ -1,12 +1,23 @@
+import 'dart:convert';
+
 import 'package:owlbot/models/word.dart';
 import 'package:owlbot/services/owlbot_secrets.dart';
+import 'package:http/http.dart' as http;
 
 class OwlBot {
   final String _token = OwlBotSecrets.token;
-  final String _baseurl = 'https://owlbot.info';
+  // API has only 1 API thing (v4/dict...) so we use it as baseurl
+  final String _baseurl = 'https://owlbot.info/api/v4/dictionary/';
 
   /// Returns Word object with definitions or null if word is not found
   Future<Word?> define(String word) async {
+    Uri url = Uri.parse(_baseurl + word);
+    http.Response response = await http.get(url);
+    if (response.statusCode == 200) {
+      // everything OK, create Word object and return it
+      return Word.fromJson(jsonDecode(response.body));
+    }
+    // if statuscode is not 200, then return null
     return null;
   }
 }
