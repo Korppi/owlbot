6928952c91964a94888d22b0c0e72a04df117887
Korppi <3602291+Korppi@users.noreply.github.com>
2021-06-17 23:38:22 +0300

    app reads token at beginning and throws error if there is no token

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:owlbot/utils/secrets.dart';
 
-void main() {
+Future<void> main() async {
+  // because main is async we need this line here
+  WidgetsFlutterBinding.ensureInitialized();
+  // init Secrects. Reads json and gets token for Owlbot API
+  await Secrets.init();
+  if (Secrets.getToken() == null) {
+    // if there is no token then somethings wrong and we cannot start app
+    throw ('no token!');
+  }
   runApp(MyApp());
 }
 
@@ -12,7 +21,14 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: Container(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Owlbot'),
+        ),
+        body: Center(
+          child: Text('Work in progress'),
+        ),
+      ),
     );
   }
 }
