353fa5a78953b5021630fe3614a9c60ea06773c4
Korppi <3602291+Korppi@users.noreply.github.com>
2021-06-17 23:36:51 +0300

    deleted as not good solution
    Going to make better way to obtain token...

--- lib/services/owlbot_secrets_sample.dart
+++
@@ -1,8 +0,0 @@
-/// Holds all secrets for OwlBot (only token)
-///
-/// Only this sample file is commited to git without real token.
-/// After pull/fork you should copy this file with new name "owlbot_secrets.dart"
-/// and add your token in there. That file is already gitignored.
-class OwlBotSecrets {
-  static final String token = '';
-}
