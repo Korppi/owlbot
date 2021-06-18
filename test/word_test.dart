6871095a32dfa061cbaa8145ff01aa191c86da0b
Korppi <3602291+Korppi@users.noreply.github.com>
2021-06-18 13:13:06 +0300

    added basic test for codemagic configuration testing
    lets remove this later

---
+++ test/word_test.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  // this is just to test codemagic stuff
+  test(
+    '1 + 1 == 2',
+    () async {
+      expect(1 + 1 == 2, true);
+    },
+  );
+}
