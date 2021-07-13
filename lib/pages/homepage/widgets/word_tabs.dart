94f631eb761c1d31fa1a4956c8790b53ae168f78
Korppi <3602291+Korppi@users.noreply.github.com>
2021-07-14 00:05:05 +0300

    wordTabs almost complete
    Missing styles, text wrapping, url opening function, tests... but overall quite alright

--- lib/pages/homepage/widgets/word_tabs.dart
+++ lib/pages/homepage/widgets/word_tabs.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:owlbot/models/definition.dart';
 import 'package:owlbot/models/word.dart';
 
 class WordTabs extends HookWidget {
@@ -10,48 +11,197 @@ class WordTabs extends HookWidget {
   Widget build(BuildContext context) {
     final pageController = usePageController();
     final tabController = useTabController(
-        initialLength: 3, // _word.definitions.length,
+        initialLength: _word.definitions.length,
         vsync: useSingleTickerProvider());
     return Expanded(
       child: ListView(
+        // TODO: keep as it is or change column here?
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            mainAxisSize: MainAxisSize.max,
-            children: <Widget>[
-              Container(
-                height: 48,
-                width: 300,
-                child: PageView(
-                  controller: pageController,
-                  children: <Widget>[
-                    Center(
-                      child: TabPageSelector(
-                        controller: tabController,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
+          _buildTabBalls(pageController, tabController),
           SizedBox(
             width: 200,
-            height: 390,
-            child: Container(
-              color: Colors.amber,
-              child: TabBarView(
-                controller: tabController,
-                children: [
-                  Text('ABC'),
-                  Text('DEF'),
-                  Text('GHI'),
-                ],
-              ),
+            height: 395,
+            child: TabBarView(
+              controller: tabController,
+              children: _buildTabs(),
             ),
           ),
         ],
       ),
+    );
+  }
+
+  _buildTabBalls(PageController pageController, TabController tabController) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisSize: MainAxisSize.max,
+      children: <Widget>[
+        Container(
+          height: 48,
+          width: 300,
+          child: PageView(
+            controller: pageController,
+            children: <Widget>[
+              Center(
+                child: TabPageSelector(
+                  controller: tabController,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  _buildTabs() {
+    List<Widget> defcards = [];
+    _word.definitions.forEach((def) {
+      defcards.add(_buildDefinitionCard(def));
+    });
+    return defcards;
+  }
+
+  _buildDefinitionCard(Definition def) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 8, right: 8, bottom: 8),
+      child: Card(
+        color: Colors.green[200],
+        child: Container(
+          child: Column(
+            children: [
+              _buildImage(def),
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(
+                    children: [
+                      Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(right: 8.0),
+                            child: Icon(Icons.title),
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('Word'), // TODO: styles
+                              Text(_word.word + ' (' + def.type + ')'),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(right: 8.0),
+                            child: Icon(Icons.record_voice_over),
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('Pronunciation'),
+                              Text(_word.pronunciation ??
+                                  ''), // TODO: check this... i think this shows weird characters (example: wrinkles)
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              Divider(
+                height: 32,
+                thickness: 2,
+                indent: 40,
+                endIndent: 200,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                    left:
+                        32.0), // TODO: this might not work with all devices...
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(right: 8.0),
+                      child: Icon(Icons.format_quote),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Example'),
+                        Text(def.example ??
+                            ''), // TODO: check long examples (owl)
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Divider(
+                height: 32,
+                thickness: 2,
+                indent: 40,
+                endIndent: 200,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                    left:
+                        32.0), // TODO: this might not work with all devices...
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(right: 8.0),
+                      child: Icon(Icons.format_quote),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Definition'),
+                        Text(
+                          def.definition,
+                        ), // TODO: check long examples (owl)
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Divider(
+                height: 32,
+                thickness: 2,
+                indent: 40,
+                endIndent: 200,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  _buildImage(Definition def) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+      child: def.imageUrl != null
+          ? GestureDetector(
+              onTap: () =>
+                  debugPrint('open url to browser!'), // TODO: implement this
+              child: CircleAvatar(
+                radius: 70,
+                backgroundImage: NetworkImage(def.imageUrl!),
+              ),
+            )
+          : Icon(
+              Icons.flutter_dash,
+              size: 140,
+            ),
     );
   }
 }
