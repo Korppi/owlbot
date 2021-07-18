a2da2b134750fc234ce9fe076adb0115bac4b1a8
Korppi <3602291+Korppi@users.noreply.github.com>
2021-07-18 23:30:29 +0300

    removed debugprints

--- lib/pages/homepage/homepage_model.dart
+++ lib/pages/homepage/homepage_model.dart
@@ -12,23 +12,16 @@ class HomepageModel extends StateNotifier<HomepageState> {
 
   /// Defines word
   Future<void> define(String? searchWord) async {
-    debugPrint('lets search word $searchWord');
     if (searchWord == null || searchWord.isEmpty) {
-      debugPrint('null or empty, so state will be init');
       state = HomepageState.init();
     } else {
-      debugPrint('not empty and not null, so state will be loading');
       state = HomepageState.loading();
-      debugPrint('lets call api');
       Word? word = await _owlBot.define(searchWord);
       if (word == null) {
-        debugPrint('api call return null, so state will be error');
         state = HomepageState.error('No word found.');
       } else {
-        debugPrint('api call return word, so state will be noError');
         state = HomepageState.noError(word);
       }
     }
-    debugPrint('search ended');
   }
 }
